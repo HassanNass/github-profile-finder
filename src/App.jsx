7bfb112a568bc903a066399aa63dd2337ca0e516
Hassan Nass <hassannass613@gmail.com>
2026-06-12 18:55:00 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return(
+    <div className="container">
+      <h1 className="main-header">GitHub Profile Finder</h1>
+    </div>
+  )
+}
+
+export default App
